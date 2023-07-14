5215c06fdb07d661ed4adf80f4715710db84bd34
Niels <niels.rogge1@gmail.com>
2023-07-14 16:06:37 +0200

    Add print statements

--- mobile_sam/modeling/tiny_vit_sam.py
+++ mobile_sam/modeling/tiny_vit_sam.py
@@ -607,6 +607,8 @@ class TinyViT(nn.Module):
         for i in range(start_i, len(self.layers)):
             layer = self.layers[i]
             x = layer(x)
+
+        print("Shape of x before neck:", x.shape)
         B,_,C=x.size()
         x = x.view(B, 64, 64, C)
         x=x.permute(0, 3, 1, 2)
